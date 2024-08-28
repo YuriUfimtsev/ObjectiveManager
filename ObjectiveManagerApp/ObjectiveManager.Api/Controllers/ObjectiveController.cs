9bf9fc1b41e903c742c196394a403afbab8162d6
yuri_ufimtsev <ufimtsev3007@gmail.com>
2024-08-29 02:13:39 +0300

    initial commit: create Api project

---
+++ ObjectiveManagerApp/ObjectiveManager.Api/Controllers/ObjectiveController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ObjectiveManager.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ObjectiveController : ControllerBase
+{
+    
+}
