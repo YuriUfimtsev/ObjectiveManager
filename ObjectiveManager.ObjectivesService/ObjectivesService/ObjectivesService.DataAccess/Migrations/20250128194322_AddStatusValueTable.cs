35ef785d2fae085b928575820cc6b6c1e91ba7b0
yuri_ufimtsev <ufimtsev3007@gmail.com>
2025-02-02 02:08:10 +0300

    [backend] add migrations

---
+++ ObjectiveManager.ObjectivesService/ObjectivesService/ObjectivesService.DataAccess/Migrations/20250128194322_AddStatusValueTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ObjectivesService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusValueTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusValues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusValues", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "StatusValues",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Создана" },
+                    { 2L, "Приостановлена" },
+                    { 3L, "Не достигнута" },
+                    { 4L, "Достигнута" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusValues");
+        }
+    }
+}
---
+++ ObjectiveManager.ObjectivesService/ObjectivesService/ObjectivesService.DataAccess/Migrations/20250201171036_AddObjectiveTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ObjectivesService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddObjectiveTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Objectives",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Definition = table.Column<string>(type: "text", nullable: false),
+                    FinalDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: true),
+                    StatusObjectId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Objectives", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Objectives_StatusObjects_StatusObjectId",
+                        column: x => x.StatusObjectId,
+                        principalTable: "StatusObjects",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Objectives_StatusObjectId",
+                table: "Objectives",
+                column: "StatusObjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Objectives");
+        }
+    }
+}
